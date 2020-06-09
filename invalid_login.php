95f8b8f211755db9fe62150a564a66e7ccfe9bab
aditbeta <aditbs00@gmail.com>
2020-06-09 13:05:14 +0700

    add alert when invalid login

---
+++ invalid_login.php
@@ -0,0 +1,27 @@
+  <!-- Invalid Login Modal-->
+  <div class="modal fade" id="invalidLoginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Message</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Username atau Password tidak valid</div>
+        <div class="modal-footer">
+          <a class="btn btn-primary" href="index.php">OK</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
